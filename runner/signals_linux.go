158d1b9774a1b86e931e7d5629534dcd50ff3f16
Pierre Souchay <p.souchay@criteo.com>
2021-01-15 11:51:03 +0100

    Always send SIGTERM to child + handle SIGQUIT

--- runner/signals_linux.go
+++ runner/signals_linux.go
@@ -1,6 +1,7 @@
 package runner
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 	"os/signal"
@@ -9,9 +10,10 @@ import (
 
 func signalWatch(cmd *exec.Cmd) {
 	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGTERM)
 	go func() {
 		sig := <-signals
-		syscall.Kill(cmd.Process.Pid, sig.(syscall.Signal))
+		fmt.Println("Received Signal", sig, "sending SIGTERM to child", cmd.Process.Pid)
+		syscall.Kill(cmd.Process.Pid, syscall.SIGTERM)
 	}()
 }
